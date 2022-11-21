b993b220a60aa1eb4259b910b6ca9519ee6ced4a
arthurtwks <lion811004@gmail.com>
2022-11-21 11:08:30 +0800

    add ejb

--- ejb-project/WebLogicEJB/src/com/pega/test/Service.java
+++ ejb-project/WebLogicEJB/src/com/pega/test/Service.java
@@ -1,5 +1,11 @@
 package com.pega.test;
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
 
+@Remote(IService.class)
+@Local(IService.IServiceLocal.class)
+@Stateless(mappedName = "Service")
 public class Service implements IService{
     @Override
     public String sayHello() throws Exception {

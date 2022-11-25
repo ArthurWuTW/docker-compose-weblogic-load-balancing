0413881ca1dc24300884ecec3fa5619db551bc42
arthurtwks <lion811004@gmail.com>
2022-11-25 18:51:05 +0800

    ejb succeed

--- ejb-project/WebLogicEJB/src/com/pega/test/ClientTest.java
+++ ejb-project/WebLogicEJB/src/com/pega/test/ClientTest.java
@@ -1,4 +1,22 @@
 package com.pega.test;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.util.Properties;
+
 public class ClientTest {
+    public static void main(String[] args){
+        System.out.println("123");
+        Properties properties = new Properties();
+        properties.put(Context.INITIAL_CONTEXT_FACTORY, "weblogic.jndi.WLInitialContextFactory");
+        properties.put(Context.PROVIDER_URL, "t3://localhost:7001");
+
+        try {
+            InitialContext ctx = new InitialContext(properties);
+            IService a = (IService)ctx.lookup("Service#com.pega.test.IService");
+            System.out.println(a.sayHello());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

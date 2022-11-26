88e78a4f87560cd235d5081297ce9fc7e965d73d
arthurtwks <lion811004@gmail.com>
2022-11-26 22:26:53 +0800

    fix wlst

--- docker/image/weblogic-admin/weblogicCreateServerClusterScript.py
+++ docker/image/weblogic-admin/weblogicCreateServerClusterScript.py
@@ -5,8 +5,7 @@ import javax.management.Attribute
 print 'starting the script .... '
 connect('weblogic','welcome1','t3://localhost:7001')
 clusters = "cluster1","cluster2"
-ms1 = {'managed1':7701,'managed2':7702,'managed3':7703, 'managed4':7704, 'managed5':7705}
-ms2 = {'managed6':7706,'managed7':7707,'managed8':7708, 'managed9':7709, 'managed10':7710}
+ms1 = {'Server-0':[7772, "172.28.1.5"],'Server-1':[7772,"172.28.1.6"]}
 clustHM = HashMap()
 edit()
 startEdit()
@@ -20,13 +19,9 @@ clus2 = clustHM.get(clusters[1])
 for m, lp in ms1.items():
     managedServer = create(m,'Server')
     print 'creating managed server '+m
-    managedServer.setListenPort(lp)
+    managedServer.setListenPort(lp[0])
+    managedServer.setListenAddress(lp[1])
     managedServer.setCluster(clus1)
-for m1, lp1 in ms2.items():
-    managedServer = create(m1,'Server')
-    print 'creating managed server '+m1
-    managedServer.setListenPort(lp1)
-    managedServer.setCluster(clus2)
 save()
 activate(block="true")
 disconnect()
